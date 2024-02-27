c145a6c32f13a4a78a14dc9d2a20824814f33310
Ashish Keshan <ashkeshan@gmail.com>
2024-02-27 13:34:06 -0500

    Update test_add.py

--- test_add.py
+++ test_add.py
@@ -3,3 +3,7 @@ from add import add_numbers
 def test_add_numbers():
     result = add_numbers(2, 3)
     assert result == 5, "Should be 5"
+
+def test_add_numbers2():
+    result = add_numbers(2, 3)
+    assert result == 5, "Should be 5"
